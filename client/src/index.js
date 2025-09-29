321a38023dfa32f3091fae01e5393f593691966f
divineProjects <divinejigu@gmail.com>
2025-09-30 01:28:39 +0200

    added an env file

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const clientId = "378724302220-19go7md8bb42reqbr79b30m2m5me01mk.apps.googleusercontent.com"; // Replace with your actual client ID
+const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
