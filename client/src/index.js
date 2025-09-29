95996e06e62d043977ab5a0113394bc578e92560
divineProjects <divinejigu@gmail.com>
2025-09-29 21:34:02 +0200

    added google-oauth and login pages

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
+const clientId = "378724302220-19go7md8bb42reqbr79b30m2m5me01mk.apps.googleusercontent.com";
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log("Client ID:", clientId);
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={clientId}>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
---
+++ client/src/pages/Register.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import GoogleLoginButton from "../components/GoogleLoginButton";
+
+function Register({ setUser }) {
+
+  return (
+    <div className="max-w-md mx-auto mt-12 p-6 bg-white/80 dark:bg-slate-800/80 rounded-2xl shadow-lg">
+      <h2 className="text-2xl font-semibold text-slate-800 dark:text-slate-100 mb-6">Register</h2>
+
+      <div className="flex justify-center">
+        <GoogleLoginButton setUser={setUser} />
+      </div>
+    </div>
+  );
+}
+
+export default Register;
